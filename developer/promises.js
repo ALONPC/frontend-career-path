f5804fa17281111390cafabb3823cdd45c3ba7bc
ALONPC <alon.berrios@gmail.com>
2021-05-12 17:42:13 -0400

    promises

---
+++ developer/promises.js
@@ -0,0 +1,52 @@
+const fetch = require("node-fetch");
+// const timeoutMessage = (seconds, message, isError) => {
+//   const wait = new Promise((res, rej) => {
+//     try {
+//       if (isError) {
+//         rej(new Error("was told to throw error"));
+//       }
+//       setTimeout(() => {
+//         res(message);
+//       }, seconds);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   });
+//   return wait.then((result) => {
+//     // result
+//     console.log(
+//       "🚀 ~ file: promises.js ~ line 15 ~ timeoutMessage ~ result",
+//       result
+//     );
+
+//     return result;
+//   });
+// };
+
+// module.exports = { timeoutMessage };
+
+const timeout = () => {
+  const wait = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Timeout!");
+    }, 1000);
+  });
+  return wait
+    .then((result) => {
+      console.log("🚀 ~ result", result);
+      return result;
+    })
+    .then((res) => res);
+};
+
+const timeoutResult = timeout();
+timeoutResult;
+
+const fetchResult = fetch("https://jsonplaceholder.typicode.com/users/1")
+  .then((response) => response.json())
+  .then((user) => {
+    console.log(user.address);
+    return user.address;
+  });
+
+fetchResult;
---
+++ index.js
@@ -0,0 +1,9 @@
+// const { timeoutMessage } = require("./developer/promises");
+
+// let result;
+
+// result = timeoutMessage(2, "Hi!");
+// result;
+
+// // result = timeoutMessage(2, "Hi!", true);
+// // result;
